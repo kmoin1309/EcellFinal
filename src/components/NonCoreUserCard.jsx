661cabc498e4d020d451364644eb2a0db18502d4
DhirajSahu18 <99193435+DhirajSahu18@users.noreply.github.com>
2023-12-24 18:07:37 +0530

    "NonCore Card Adjusted"

--- src/components/NonCoreUserCard.jsx
+++ src/components/NonCoreUserCard.jsx
@@ -1,5 +1,99 @@
+// import React from "react";
+// import {  UserDefault } from "../assets";
+
+// const NonCoreUserCard = ({ candidate }) => {
+//   const cardStyle = {
+//     backgroundColor: "#2C1579",
+//     transition: "background-color 0.3s",
+//   };
+
+//   if (candidate.hovercolor) {
+//     cardStyle["backgroundColor"] = "#2C1579"; // Set the default background color
+
+//     cardStyle[":hover"] = {
+//       backgroundColor: candidate.hovercolor, // Change background color on hover
+//     };
+//   }
+
+//   return (
+//     <div
+//       className="w-80 h-[120px] rounded-lg overflow-hidden shadow-lg bg-[#2C1579] m-4 p-4 flex"
+//       style={cardStyle}
+//     >
+//       {/* User Image */}
+//       <div className="w-2/5">
+//         <img
+//           src={candidate.img ? candidate.img : UserDefault}
+//           alt={candidate.name}
+//           className="w-full rounded-full h-full"
+//         />
+//       </div>
+
+//       {/* User Details */}
+//       <div className="w-3/5 p-2 flex flex-col justify-between">
+//         {/* User Name */}
+//         <div>
+//           <p className="text-md text-white font-semibold truncate  hover:text-clip">{candidate.name}</p>
+//           <p className="text-sm text-white">{candidate.position}</p>
+//         </div>
+
+//         {/* Social Media Buttons */}
+//         <div className="flex justify-start gap-2">
+//           {/* LinkedIn icon */}
+//           {candidate.linkedin && (
+//             <a
+//               href={candidate.linkedin}
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img
+//                 src="imported-svgs/linkedin.svg" // Replace with the actual LinkedIn icon image URL
+//                 alt="LinkedIn"
+//                 className="w-6 h-6"
+//                 style={{ filter: 'brightness(0) invert(1)' }}
+//               />
+//             </a>
+//           )}
+
+//           {/* GitHub icon */}
+//           {candidate.github && (
+//             <a href={candidate.github} target="_blank" rel="noopener noreferrer">
+//               <img
+//                 src="imported-svgs/github.svg" // Replace with the actual GitHub icon image URL
+//                 alt="GitHub"
+//                 className="w-6 h-6"
+//                 style={{ filter: 'brightness(0) invert(1)' }}
+//               />
+//             </a>
+//             )}
+
+//           {/* Instagram icon */}
+//           {candidate.instagram && (
+//             <a
+//               href={candidate.instagram}
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <img
+//                 src="imported-svgs/instagram.svg" // Replace with the actual Instagram icon image URL
+//                 alt="Instagram"
+//                 className="w-6 h-6"
+//                 style={{ filter: 'brightness(0) invert(1)' }}
+//               />
+//             </a>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+
+//   );
+// };
+
+// export default NonCoreUserCard;
+
+
 import React from "react";
-import {  UserDefault } from "../assets";
+import { UserDefault } from "../assets";
 
 const NonCoreUserCard = ({ candidate }) => {
   const cardStyle = {
@@ -17,15 +111,16 @@ const NonCoreUserCard = ({ candidate }) => {
 
   return (
     <div
-      className="w-80 h-[120px] rounded-lg overflow-hidden shadow-lg bg-[#2C1579] m-4 p-4 flex"
+      className="w-80 h-[170px] rounded-lg overflow-hidden shadow-lg bg-[#2C1579] m-4 p-4 flex"
       style={cardStyle}
     >
       {/* User Image */}
-      <div className="w-2/5">
+      <div className="w-2/5 flex justify-center items-center">
         <img
           src={candidate.img ? candidate.img : UserDefault}
           alt={candidate.name}
-          className="w-full rounded-full h-full"
+          className="w-full h-auto rounded-full max-h-full"
+          style={{ objectFit: 'cover' }}
         />
       </div>
 
@@ -33,7 +128,9 @@ const NonCoreUserCard = ({ candidate }) => {
       <div className="w-3/5 p-2 flex flex-col justify-between">
         {/* User Name */}
         <div>
-          <p className="text-md text-white font-semibold truncate  hover:text-clip">{candidate.name}</p>
+          <p className="text-md text-white font-semibold truncate hover:text-clip">
+            {candidate.name}
+          </p>
           <p className="text-sm text-white">{candidate.position}</p>
         </div>
 
@@ -65,7 +162,7 @@ const NonCoreUserCard = ({ candidate }) => {
                 style={{ filter: 'brightness(0) invert(1)' }}
               />
             </a>
-            )}
+          )}
 
           {/* Instagram icon */}
           {candidate.instagram && (
@@ -85,7 +182,6 @@ const NonCoreUserCard = ({ candidate }) => {
         </div>
       </div>
     </div>
-
   );
 };
 
